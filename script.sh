945d63d0e84bbf37b96c87ef60b484d801c6d7c6
saishiva0603 <52260218+saishiva0603@users.noreply.github.com>
2019-07-24 19:53:36 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 URL=https://github.com/saishiva0603/project4/
 FIRSTNAME=ansible-
 LASTNAME=$(git ls-remote $URL HEAD|cut -c 1-40)
@@ -13,3 +12,6 @@ echo $FULLNAME
 cd /etc/ansible/
 ansible-playbook clone.yml
 
+
+//this command is for ssh of controller machine from jenkins execute shell
+ssh -i /home/ubuntu/key -o BatchMode=yes -o StrictHostKeyChecking=no ubuntu@10.10.0.218 'bash -s' < script.sh
