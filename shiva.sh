3d2149a9b19ce83957f95a4e11bdc5c78a9aaec8
saishiva0603 <52260218+saishiva0603@users.noreply.github.com>
2019-07-29 08:43:37 -0400

    Update shiva.sh

--- shiva.sh
+++ shiva.sh
@@ -13,5 +13,5 @@ cd /etc/ansible/
 ansible-playbook clone.yml
 
 
-//this command is for ssh of controller machine from jenkins execute shell
+#this command is for ssh of controller machine from jenkins execute shell
 ssh -i /home/ubuntu/key -o BatchMode=yes -o StrictHostKeyChecking=no ubuntu@10.10.0.218 'bash -s' < script.sh
