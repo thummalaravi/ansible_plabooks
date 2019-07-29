5d74d78a13403cec09ae50e96a92e0fe472c8555
saishiva0603 <52260218+saishiva0603@users.noreply.github.com>
2019-07-29 12:58:59 -0400

    Update Project16.tf

--- Project16.tf
+++ Project16.tf
@@ -312,3 +312,63 @@ resource "aws_route53_record" "project16" {
    evaluate_target_health = true
  }
 }
+
+
+
+
+resource "aws_instance" "acess" {
+  ami           = "ami-05c1fa8df71875112"
+  instance_type = "t2.micro"
+  associate_public_ip_address = "true"
+  subnet_id = "${aws_subnet.project16_1b.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
+  #key_name = "${var.key_name}"
+  tags=  {
+        Name = "acess"
+  }
+}
+
+resource "aws_instance" "control" {
+  ami           = "ami-05c1fa8df71875112"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.project16_2a.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
+  #key_name = "${var.key_name}"
+  tags = {
+        Name = "control"
+  }
+}
+
+resource "aws_instance" "web01" {
+  ami           = "ami-05c1fa8df71875112"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.project16_2c.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
+  #key_name = "${var.key_name}"
+  tags =  {
+        Name = "web01"
+  }
+}
+
+resource "aws_instance" "web02" {
+  ami           = "ami-05c1fa8df71875112"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.project16_2c.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
+  #key_name = "${var.key_name}"
+  tags = {
+        Name = "web02"
+  }
+}
+
+resource "aws_instance" "db_server" {
+  ami           = "ami-05c1fa8df71875112"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.project16_2b.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
+  #key_name = "${var.key_name}"
+  tags = {
+        Name = "db_server"
+  }
+}
+
