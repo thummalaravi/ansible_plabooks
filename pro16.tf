38373628c90fd8ee6cbdfce52d8c3aeee5690b72
saishiva0603 <52260218+saishiva0603@users.noreply.github.com>
2019-07-29 12:58:20 -0400

    Update pro16.tf

--- pro16.tf
+++ pro16.tf
@@ -2,11 +2,11 @@
 
 
 resource "aws_instance" "acess" {
-  ami           = "ami-08fba854c7827042b"
+  ami           = "ami-05c1fa8df71875112"
   instance_type = "t2.micro"
   associate_public_ip_address = "true"
-  subnet_id = "${aws_subnet.project16_public_subnet_a1.id}"
-  vpc_security_group_ids = ["${aws_security_group.project16_sg.id}"]
+  subnet_id = "${aws_subnet.project16_1b.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
   #key_name = "${var.key_name}"
   tags=  {
         Name = "acess"
@@ -14,10 +14,10 @@ resource "aws_instance" "acess" {
 }
 
 resource "aws_instance" "control" {
-  ami           = "ami-08fba854c7827042b"
+  ami           = "ami-05c1fa8df71875112"
   instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.project16_pvt_subnet_a2.id}"
-  vpc_security_group_ids = ["${aws_security_group.project16_sg.id}"]
+  subnet_id = "${aws_subnet.project16_2a.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
   #key_name = "${var.key_name}"
   tags = {
         Name = "control"
@@ -25,10 +25,10 @@ resource "aws_instance" "control" {
 }
 
 resource "aws_instance" "web01" {
-  ami           = "ami-08fba854c7827042b"
+  ami           = "ami-05c1fa8df71875112"
   instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.project16_pvt_subnet_b2.id}"
-  vpc_security_group_ids = ["${aws_security_group.project16_sg.id}"]
+  subnet_id = "${aws_subnet.project16_2c.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
   #key_name = "${var.key_name}"
   tags =  {
         Name = "web01"
@@ -36,10 +36,10 @@ resource "aws_instance" "web01" {
 }
 
 resource "aws_instance" "web02" {
-  ami           = "ami-08fba854c7827042b"
+  ami           = "ami-05c1fa8df71875112"
   instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.project16_pvt_subnet_c2.id}"
-  vpc_security_group_ids = ["${aws_security_group.project16_sg.id}"]
+  subnet_id = "${aws_subnet.project16_2c.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
   #key_name = "${var.key_name}"
   tags = {
         Name = "web02"
@@ -47,10 +47,10 @@ resource "aws_instance" "web02" {
 }
 
 resource "aws_instance" "db_server" {
-  ami           = "ami-08fba854c7827042b"
+  ami           = "ami-05c1fa8df71875112"
   instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.project16_pvt_subnet_b2.id}"
-  vpc_security_group_ids = ["${aws_security_group.project16_sg.id}"]
+  subnet_id = "${aws_subnet.project16_2b.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
   #key_name = "${var.key_name}"
   tags = {
         Name = "db_server"
